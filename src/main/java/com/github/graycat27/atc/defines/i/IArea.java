d9d02d6ca45e4e649ae1f9c4bef900c54a863b9b
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-04-17 17:43:53 +0900

    Areaへのメソッド追加

--- src/main/java/com/github/graycat27/atc/defines/i/IArea.java
+++ src/main/java/com/github/graycat27/atc/defines/i/IArea.java
@@ -5,6 +5,11 @@ import com.github.graycat27.atc.utils.ICloneable;
 /** 空域、地表面域など、x-y-zの範囲を持つ */
 public interface IArea extends ICloneable {
 
+
+    /** 指定地点がこのエリア内であるか判定する
+     * @return エリア内に含まれる場合<code>true</code> 含まれない場合<code>false</code> */
+    boolean isIn(IPoint point);
+
     @Override
     IArea clone();
 }
