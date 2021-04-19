ff3f77f9137cfa6608999c9d698933774e0c8cf3
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-04-19 10:29:56 +0900

    TODO の明記

--- src/main/java/com/github/graycat27/atc/handler/command/action/CommandAirport.java
+++ src/main/java/com/github/graycat27/atc/handler/command/action/CommandAirport.java
@@ -2,14 +2,14 @@ package com.github.graycat27.atc.handler.command.action;
 
 public class CommandAirport{
     public static void add(String name) {
-
+        //TODO make this
     }
 
     public static void mod(String[] args) {
-
+        //TODO make this
     }
 
     public static void info(String param) {
-
+        //TODO make this
     }
 }
--- src/main/java/com/github/graycat27/atc/handler/command/action/CommandArea.java
+++ src/main/java/com/github/graycat27/atc/handler/command/action/CommandArea.java
@@ -3,14 +3,14 @@ package com.github.graycat27.atc.handler.command.action;
 public class CommandArea {
 
     public static void add(String freq){
-
+        //TODO make this
     }
 
     public static void mod(String[] args){
-
+        //TODO make this
     }
 
     public static void info(String param){
-
+        //TODO make this
     }
 }
