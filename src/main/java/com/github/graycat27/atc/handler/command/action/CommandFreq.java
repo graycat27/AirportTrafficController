e0c3a51621013bc0078d1c4f8a618efe16cad52c
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-04-19 21:22:15 +0900

    コマンド /atc freq cut の実装

--- src/main/java/com/github/graycat27/atc/handler/command/action/CommandFreq.java
+++ src/main/java/com/github/graycat27/atc/handler/command/action/CommandFreq.java
@@ -2,8 +2,8 @@ package com.github.graycat27.atc.handler.command.action;
 
 import com.github.graycat27.atc.AirportTrafficController;
 import com.github.graycat27.atc.components.FrequencyUtil;
-import com.github.graycat27.atc.defines.airport.Airport;
-import com.github.ucchyocean.lc.channel.ChannelPlayer;
+import com.github.graycat27.atc.components.LunaChatUtil;
+import com.github.graycat27.atc.consts.LcConst;
 import com.github.ucchyocean.lc3.channel.Channel;
 import com.github.ucchyocean.lc3.member.ChannelMember;
 import org.bukkit.command.CommandSender;
@@ -40,8 +40,37 @@ public class CommandFreq {
     }
 
     public static void runCutCommand(CommandSender sender, String targetParam){
-        //TODO make this
-        sendMessage(sender, "cut command is unavailable");
+        if(!(sender instanceof Player)){
+            sendMessage(sender, "this command is only allowed for in-game player");
+            return;
+        }
+
+        Channel targetChannel = null;
+        String playerName = sender.getName();
+        if(targetParam == null || targetParam.length() == 0){
+            Channel curCh = AirportTrafficController.getLcApi().getDefaultChannel(playerName);
+            if(curCh != null) {
+                targetChannel = curCh;
+            }
+        }else{
+            String targetFreq = targetParam.replace("\\.", LcConst.ATC_CHANNEL_FREQ_CHAR);
+            String nameByParam = FrequencyUtil.getChannelName(targetFreq);
+            Channel channel = AirportTrafficController.getLcApi().getChannel(nameByParam);
+            if(channel != null){
+                targetChannel = channel;
+            }
+        }
+        if(targetChannel == null){
+            sendMessage(sender, "that ATC frequency is not found");
+            return;
+        }
+        if(!LunaChatUtil.isAtcChannel(targetChannel)){
+            sendMessage(sender, "you can not leave by this command :"+ targetChannel.getName());
+            return;
+        }
+
+        targetChannel.removeMember(ChannelMember.getChannelMember(sender.getName()));
+
     }
 
 }
