8873c7e8afd093224cbd46c0b8654b9461723ed8
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-05-10 12:30:52 +0900

    airportにgetterを作成

--- src/main/java/com/github/graycat27/atc/defines/airport/Airport.java
+++ src/main/java/com/github/graycat27/atc/defines/airport/Airport.java
@@ -1,7 +1,6 @@
 package com.github.graycat27.atc.defines.airport;
 
 import com.github.graycat27.atc.defines.atc.ATCControl;
-import com.github.graycat27.atc.defines.sky.ATCArea;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +12,7 @@ public class Airport {
 
     //定義情報
     /** 空港名 */
-    private String name = "";
+    private String name;
     public String getName(){
         return name;
     }
@@ -21,15 +20,27 @@ public class Airport {
     //施設系
     /** 滑走路 */
     private List<Runway> runways;
+    public List<Runway> getRunways(){
+        return new ArrayList<>(runways);
+    }
 
     /** 誘導路 */
     private List<Taxiway> taxiways;
+    public List<Taxiway> getTaxiways(){
+        return new ArrayList<>(taxiways);
+    }
 
     /** 駐機位置（スポット） */
     private List<Spot> spots;
+    public List<Spot> getSpots(){
+        return new ArrayList<>(spots);
+    }
 
     /** 管制塔 */
     private Tower tower;
+    public Tower getTower(){
+        return tower.clone();
+    }
 
     //管制系
     /** 管制空域 */
@@ -41,6 +52,15 @@ public class Airport {
 
 
     //コンストラクタ
+    private Airport(String name, List<Runway> runway, List<Taxiway> taxiway,
+                    List<Spot> spot, Tower tower, List<ATCControl> contol){
+        this.name = name;
+        this.runways = runway;
+        this.taxiways = taxiway;
+        this.spots = spot;
+        this.tower = tower;
+        this.areas = contol;
+    }
 
 
     //メソッド
