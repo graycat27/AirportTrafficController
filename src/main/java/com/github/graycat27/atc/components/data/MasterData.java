ff6593de7885fa76afe6469bc15cbcee4943371f
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-05-11 14:38:38 +0900

    フィールドの作成

--- src/main/java/com/github/graycat27/atc/components/data/MasterData.java
+++ src/main/java/com/github/graycat27/atc/components/data/MasterData.java
@@ -1,20 +1,29 @@
 package com.github.graycat27.atc.components.data;
 
+import com.github.graycat27.atc.defines.airport.Airport;
 import com.github.graycat27.atc.defines.i.IMaster;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MasterData implements IMaster {
 
     //フィールド
-
+    List<Airport> airportList;
 
     //コンストラクタ
+    private MasterData(List<Airport> apList){
+        this.airportList = apList;
+    }
 
+    public MasterData(){
+        airportList = new ArrayList<>();
+    }
 
     //メソッド
 
     @Override
     public MasterData clone() {
-        //FIXME
-        return null;
+        return new MasterData(new ArrayList<>(airportList));
     }
 }
