cc176c7039f3fe68cec021f39487e411bc6605c5
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-05-11 13:16:16 +0900

    ATCControlのfinalフィールド化

--- src/main/java/com/github/graycat27/atc/defines/atc/ATCControl.java
+++ src/main/java/com/github/graycat27/atc/defines/atc/ATCControl.java
@@ -1,30 +1,42 @@
 package com.github.graycat27.atc.defines.atc;
 
 import com.github.graycat27.atc.consts.Control;
-import com.github.graycat27.atc.defines.i.IArea;
 import com.github.graycat27.atc.defines.i.IFrequency;
+import com.github.graycat27.atc.defines.i.IMaster;
 import com.github.graycat27.atc.defines.sky.ATCArea;
 
-public class ATCControl {
+public class ATCControl implements IMaster {
 
     //周波数と、空域の情報を持たせる
     //参加しているユーザ情報はLCのチャンネルメンバー。
     //管制官役のプレイヤーを将来的には持たせたい
 
     /** 管制の役割 */
-    private Control control;
+    private final Control control;
     public Control getControl(){
         return control;
     }
 
-    private IFrequency frequency;
+    private final IFrequency frequency;
     public IFrequency getFrequency(){
         return frequency;
     }
 
-    private ATCArea area;
+    private final ATCArea area;
     public ATCArea getArea(){
         return area;
     }
 
+    //コンストラクタ
+    public ATCControl(Control c, IFrequency f, ATCArea a){
+        this.control = c;
+        this.frequency = f;
+        this.area = a;
+    }
+
+    //メソッド
+    @Override
+    public ATCControl clone(){
+        return new ATCControl(this.getControl(), this.getFrequency(), this.getArea());
+    }
 }
