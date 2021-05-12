1c2389f32dc6407601eed80e7c08b726b416aee8
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-05-12 13:33:33 +0900

    addメソッドの実装

--- src/main/java/com/github/graycat27/atc/components/data/DataUtil.java
+++ src/main/java/com/github/graycat27/atc/components/data/DataUtil.java
@@ -2,6 +2,10 @@ package com.github.graycat27.atc.components.data;
 
 import com.github.graycat27.atc.AirportTrafficController;
 import com.github.graycat27.atc.defines.airport.Airport;
+import com.github.graycat27.atc.defines.airport.Runway;
+import com.github.graycat27.atc.defines.airport.Spot;
+import com.github.graycat27.atc.defines.airport.Taxiway;
+import com.github.graycat27.atc.defines.atc.ATCControl;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -10,7 +14,7 @@ import java.util.List;
 
 /** データを操作するstaticメソッドを持つUtilクラス<br>
  * 空港名をキーに、各種操作をするAPIを持つ */
-public class DataUtil {
+public final class DataUtil {
 
     // フィールド
 
@@ -43,6 +47,21 @@ public class DataUtil {
         return data().getAirportList().get(airportName).clone();
     }
 
+    public static void addRunwayToAirport(String airportName, Runway newOne){
+        data().getAirportList().get(airportName).addRunway(newOne);
+    }
+
+    public static void addTaxiwayToAirport(String airportName, Taxiway newOne){
+        data().getAirportList().get(airportName).addTaxiway(newOne);
+    }
+
+    public static void addSpotToAirport(String airportName, Spot newOne){
+        data().getAirportList().get(airportName).addSpot(newOne);
+    }
+
+    public static void addControlToAirport(String airportName, ATCControl newOne){
+        data().getAirportList().get(airportName).addControl(newOne);
+    }
 
 
 
