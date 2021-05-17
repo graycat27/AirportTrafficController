1f68b8483ca747fdd24d7c9722ef097e81572ff6
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-05-17 14:13:01 +0900

    helpコマンドの実装

--- src/main/java/com/github/graycat27/atc/handler/command/action/CommandHelp.java
+++ src/main/java/com/github/graycat27/atc/handler/command/action/CommandHelp.java
@@ -1,5 +1,6 @@
 package com.github.graycat27.atc.handler.command.action;
 
+import com.github.graycat27.atc.consts.CommandWord;
 import com.github.graycat27.atc.handler.command.AtcCommandHandler;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -8,7 +9,20 @@ public class CommandHelp {
     private CommandHelp(){}
 
     public static void showHelp(CommandSender sender){
-        //TODO コマンドヘルプ一覧を送出する
-        AtcCommandHandler.sendMessage(sender, "help will show in the future");
+        AtcCommandHandler.sendMessage(sender, "ATC commands");
+        // /atc freq
+        String atcFreq = CommandWord.ATC + " " + CommandWord.Freq.FREQ + " ";
+        AtcCommandHandler.sendMessage(sender, "[/"+ atcFreq + CommandWord.Freq.MONITOR + " 123.4]" +
+                " 周波数123.4に接続します");
+        AtcCommandHandler.sendMessage(sender, "[/"+ atcFreq+ CommandWord.Freq.CUT + " 123.4]" +
+                " 周波数123.4から切断します");
+
+        // /atc manage
+        String atcManage = CommandWord.ATC + " " + CommandWord.Manage.MANAGE + " ";
+        String actions = CommandWord.Manage.ADD + "|" + CommandWord.Manage.MODIFY + "|" + CommandWord.Manage.INFO;
+        String targets = CommandWord.Target.AIRPORT + "|" + CommandWord.Target.AREA;
+        AtcCommandHandler.sendMessage(sender, "[/" + atcManage + actions + " " + targets + "]" +
+                " 管理コマンド群");
+
     }
 }
