a748387d9d561d42204e60a60485db7cf2f50e49
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-05-20 15:39:01 +0900

    permission制限の実装

--- src/main/java/com/github/graycat27/atc/consts/PermissionNode.java
+++ src/main/java/com/github/graycat27/atc/consts/PermissionNode.java
@@ -2,14 +2,18 @@ package com.github.graycat27.atc.consts;
 
 public class PermissionNode {
 
+    private PermissionNode(){
+        /* not to be instance */
+    }
+
     /** permission for changing commands like
      * <code>/atc manage add</code>or<code>/atc manage mod</code> */
-    public final String ATC_MANAGE_CHANGE = "atc.manage.change";
+    public static final String ATC_MANAGE_CHANGE = "atc.manage.change";
     /** permission for view info command
      * <code>/atc manage info</code> */
-    public final String ATC_MANAGE_INFO = "atc.manage.info";
+    public static final String ATC_MANAGE_INFO = "atc.manage.info";
 
     /** permission for join or leave ATC channel */
-    public final String ATC_FREQ = "atc.freq";
+    public static final String ATC_FREQ = "atc.freq";
 
 }
--- src/main/java/com/github/graycat27/atc/handler/command/AtcCommandHandler.java
+++ src/main/java/com/github/graycat27/atc/handler/command/AtcCommandHandler.java
@@ -1,10 +1,12 @@
 package com.github.graycat27.atc.handler.command;
 
 import com.github.graycat27.atc.consts.CommandWord;
+import com.github.graycat27.atc.consts.PermissionNode;
 import com.github.graycat27.atc.handler.command.action.CommandAirport;
 import com.github.graycat27.atc.handler.command.action.CommandArea;
 import com.github.graycat27.atc.handler.command.action.CommandFreq;
 import com.github.graycat27.atc.handler.command.action.CommandHelp;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -23,6 +25,8 @@ public class AtcCommandHandler implements CommandExecutor, TabCompleter {
 
     // フィールド
     private static Plugin plugin;
+
+    private final String permissionErrorMsg = ChatColor.RED + "you don`t have permission";
 
     // コンストラクタ
     public AtcCommandHandler(Plugin plugin){
@@ -47,6 +51,9 @@ public class AtcCommandHandler implements CommandExecutor, TabCompleter {
         }
         /* /atc freq xxx */
         if(CommandWord.Freq.FREQ.equalsIgnoreCase(args[0])){
+            if(!sender.hasPermission(PermissionNode.ATC_FREQ)){
+                sendMessageNoPermission(sender);
+            }
             if(args.length < 2){
                 sendMessage(sender, "less parameter for command [/atc freq]");
                 this.sendMessageUnknownCommand(sender);
@@ -69,6 +76,9 @@ public class AtcCommandHandler implements CommandExecutor, TabCompleter {
         if (CommandWord.Manage.MANAGE.equalsIgnoreCase(args[0])){
             /* /atc manage add xxx */
             if (CommandWord.Manage.ADD.equalsIgnoreCase(args[1])) {
+                if(!sender.hasPermission(PermissionNode.ATC_MANAGE_CHANGE)){
+                    sendMessageNoPermission(sender);
+                }
                 if (args.length < 3) {
                     sendMessage(sender, "less parameter for command [/atc manage add]");
                     this.sendMessageUnknownCommand(sender);
@@ -96,6 +106,9 @@ public class AtcCommandHandler implements CommandExecutor, TabCompleter {
 
             /* /atc manage mod xxx */
             if (CommandWord.Manage.MODIFY.equalsIgnoreCase(args[1])) {
+                if(!sender.hasPermission(PermissionNode.ATC_MANAGE_CHANGE)){
+                    sendMessageNoPermission(sender);
+                }
                 if (args.length < 3) {
                     sendMessage(sender, "less parameter for command [/atc manage mod]");
                     this.sendMessageUnknownCommand(sender);
@@ -121,6 +134,9 @@ public class AtcCommandHandler implements CommandExecutor, TabCompleter {
 
             /* /atc manage info xxx */
             if (CommandWord.Manage.INFO.equalsIgnoreCase(args[1])) {
+                if(!sender.hasPermission(PermissionNode.ATC_FREQ)){
+                    sendMessageNoPermission(sender);
+                }
                 if (args.length < 3) {
                     sendMessage(sender, "less parameter for command [/atc manage info]");
                     this.sendMessageUnknownCommand(sender);
@@ -148,6 +164,10 @@ public class AtcCommandHandler implements CommandExecutor, TabCompleter {
 
     private void sendMessageUnknownCommand(CommandSender sender){
         sendMessage(sender, "wrong command. use [/atc help]");
+    }
+
+    private void sendMessageNoPermission(CommandSender sender){
+        sendMessage(sender, permissionErrorMsg);
     }
 
     /**
