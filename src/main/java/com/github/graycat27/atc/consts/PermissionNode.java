45837d6dc795597d6fd44d43061ebc523e49187a
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-05-20 15:19:56 +0900

    Permission一覧定数クラス作成

---
+++ src/main/java/com/github/graycat27/atc/consts/PermissionNode.java
@@ -0,0 +1,15 @@
+package com.github.graycat27.atc.consts;
+
+public class PermissionNode {
+
+    /** permission for changing commands like
+     * <code>/atc manage add</code>or<code>/atc manage mod</code> */
+    public final String ATC_MANAGE_CHANGE = "atc.manage.change";
+    /** permission for view info command
+     * <code>/atc manage info</code> */
+    public final String ATC_MANAGE_INFO = "atc.manage.info";
+
+    /** permission for join or leave ATC channel */
+    public final String ATC_FREQ = "atc.freq";
+
+}
