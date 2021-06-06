57cab13543aafdb94237c209e63b7f7f56e85485
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-06-06 15:12:14 +0900

    プロパティファイルの作成と読み出しの実装

--- src/main/java/com/github/graycat27/atc/consts/Property.java
+++ src/main/java/com/github/graycat27/atc/consts/Property.java
@@ -12,10 +12,14 @@ public class Property {
     /** 辞書ファイルのファイル名 */
     public static final String FILE_NAME_DICTIONARY = "atc_dic.prop";
 
+    /** プロパティファイルのファイル名 */
+    public static final String FILE_NAME_PROPERTY = "atc_property.property";
 
     //プロパティから読みだすキー値
-
     /** 管制設定のできるワールド名（サーバ内で単一指定） */
     public static final String WORLD_NAME = "world_name";
+
+    /** レーダーの照射秒 */
+    public static final String RADAR_SECONDS = "radar_seconds";
 
 }
--- src/main/java/com/github/graycat27/atc/setting/PropertySettings.java
+++ src/main/java/com/github/graycat27/atc/setting/PropertySettings.java
@@ -2,17 +2,34 @@ package com.github.graycat27.atc.setting;
 
 import com.github.graycat27.atc.consts.Property;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
 /** プロパティから読み出した値を持つ */
 public class PropertySettings {
 
-    public PropertySettings() {
+    private static Properties props = new Properties();
 
+    private PropertySettings() { /* インスタンス化防止 */ }
+
+    public static void readProps(){
+        Path propFilePath = Path.of(Property.FILE_PATH_DIR, Property.FILE_NAME_PROPERTY);
+        try {
+            props.load(Files.newBufferedReader(propFilePath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static String worldName(){
-        //FIXME read prop and return value
-        //Property.WORLD_NAME;
-        return "world";
+        String worldName = (String) props.get(Property.WORLD_NAME);
+        return worldName != null ? worldName : "world";
+    }
+
+    public static int radarSeconds(){
+        return Integer.parseInt((String) props.get(Property.RADAR_SECONDS));
     }
 
 }
