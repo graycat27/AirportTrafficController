ab6ba21c78e84d1530c7ee0b76415c142055c31a
graycat27 <64903817+graycat27@users.noreply.github.com>
2021-06-14 09:56:19 +0900

    不要なラッピング

--- src/main/java/com/github/graycat27/atc/handler/command/action/CommandArea.java
+++ src/main/java/com/github/graycat27/atc/handler/command/action/CommandArea.java
@@ -60,7 +60,7 @@ public class CommandArea {
             argCenterY = argCenterPos[1];
             argCenterZ = argCenterPos[2];
         }
-        Integer argIntCenterX, argIntCenterY, argIntCenterZ;
+        int argIntCenterX, argIntCenterY, argIntCenterZ;
         try{
             argIntCenterX = Integer.parseInt(argCenterX);
             argIntCenterY = Integer.parseInt(argCenterY);
